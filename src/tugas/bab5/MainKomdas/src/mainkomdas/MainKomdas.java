cdc3a124f1967cbf6570eb45c893b8224968aedc
Johan Fajar <johanrubik@gmail.com>
2016-04-26 17:28:19 +0700

    edit5

--- src/tugas/bab5/MainKomdas/src/mainkomdas/MainKomdas.java
+++ src/tugas/bab5/MainKomdas/src/mainkomdas/MainKomdas.java
@@ -11,6 +11,21 @@ public class MainKomdas {
         for (;;) {
             System.out.println("");
             System.out.println(" >> CV.Labkomdas << ");
+            System.out.println("======= MENU =======");
+            System.out.println("1. Pembelian Jaket A");
+            System.out.println("2. Pembelian Jaket B");
+            System.out.println("3. Pembelian Jaket C");
+            System.out.println("4. Keluar Program ");
+            System.out.print("Masukkan Pilihan : ");
+            pil = in.nextInt();
+            switch (pil) {
+                case 1:
+                    System.out.println("");
+                    System.out.println("Masukkan jumlah jaker : ");
+                    jml = in.nextInt();
+                    Komdas.hitungJaketA(jml);
+                    break;
+            }
         }
         
         
