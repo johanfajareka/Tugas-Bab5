cb564f409b60850625b8e0265372e006c2f860ab
Johan Fajar <johanrubik@gmail.com>
2016-04-26 14:39:07 +0700

    edit3

--- src/tugas/bab5/MainHitungan/src/mainhitungan/Hitungan.java
+++ src/tugas/bab5/MainHitungan/src/mainhitungan/Hitungan.java
@@ -1,26 +1,36 @@
 package mainhitungan;
 
 public class Hitungan {
+
     private static double a;
     private static int b;
-
+    
     public Hitungan() {
+        
+    }    
 
-    } 
-    public static void penjumlahan(int x, int y){
+    public static void penjumlahan(int x, int y) {
         b = x + y;
-        System.out.println("Hasil penjumlahan = " +b);
+        System.out.println("Hasil penjumlahan = " + b);
     }
-    public static void pengurangan(int x, int y){
+
+    public static void pengurangan(int x, int y) {
         b = x - y;
-        System.out.println("Hasil penjumlahan = " +b);
+        System.out.println("Hasil penjumlahan = " + b);
     }
+
     public void perkalian(int x, int y) {
         b = x * y;
-        System.out.println("Hasil perkalian = " +b);
+        System.out.println("Hasil perkalian = " + b);
     }
-    public void pembagian(double x, double y){
+
+    public void pembagian(double x, double y) {
         a = x / y;
-        
+        Hitungan hitung = new Hitungan();
+        hitung.sederhana(a);
+    }
+
+    public void sederhana(double c) {
+        System.out.printf("Hasil pembagian = %.2f", a);
     }
 }
