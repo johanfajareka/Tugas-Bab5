f2780c94818c3b2fb93ee013c766565819d95f61
Johan Fajar <johanrubik@gmail.com>
2016-04-26 17:26:21 +0700

    edit4

--- src/tugas/bab5/MainKomdas/src/mainkomdas/MainKomdas.java
+++ src/tugas/bab5/MainKomdas/src/mainkomdas/MainKomdas.java
@@ -1,9 +1,17 @@
 package mainkomdas;
 
+import java.util.Scanner;
+
 public class MainKomdas {
 
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int pil,jml;
         
+        for (;;) {
+            System.out.println("");
+            System.out.println(" >> CV.Labkomdas << ");
+        }
         
         
     }
