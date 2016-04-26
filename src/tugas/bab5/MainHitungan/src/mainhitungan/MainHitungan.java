0274db1fbf0b0e66abf97ad1b506af87f905ab00
Johan Fajar <johanrubik@gmail.com>
2016-04-26 14:45:49 +0700

    edit6

--- src/tugas/bab5/MainHitungan/src/mainhitungan/MainHitungan.java
+++ src/tugas/bab5/MainHitungan/src/mainhitungan/MainHitungan.java
@@ -7,8 +7,8 @@ public class MainHitungan {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         Hitungan h = new Hitungan();
-        int a,b,pil;
-        double x,y;
+        int a, b, pil;
+        double x, y;
         for (;;) {
             System.out.println(" >> Menu Perhitungan << ");
             System.out.println("    1. Penjumlahan");
@@ -18,10 +18,25 @@ public class MainHitungan {
             System.out.println("    5. Keluar");
             System.out.print("Masukkan Pilihan anda : ");
             pil = in.nextInt();
-            switch (pil){
-                
+            switch (pil) {
+                case 1:
+                    System.out.println("");
+                    System.out.print("Masukkan nilai 1 : ");
+                    a = in.nextInt();
+                    System.out.print("Masukkan nilai 2 : ");
+                    b = in.nextInt();
+                    Hitungan.penjumlahan(a, b);
+                    break;
+                case 2:
+                    System.out.println("");
+                    System.out.print("Masukkan nilai 1 : ");
+                    a = in.nextInt();
+                    System.out.print("Masukkan nilai 2 : ");
+                    b = in.nextInt();
+                    Hitungan.pengurangan(a, b);
+                    break;
             }
         }
     }
-    
+
 }
