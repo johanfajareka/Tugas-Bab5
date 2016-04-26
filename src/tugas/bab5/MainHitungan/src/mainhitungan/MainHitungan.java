28b51c3174de1fd5fe1fab465390ac24b88c184a
Johan Fajar <johanrubik@gmail.com>
2016-04-26 14:40:26 +0700

    edit4

--- src/tugas/bab5/MainHitungan/src/mainhitungan/MainHitungan.java
+++ src/tugas/bab5/MainHitungan/src/mainhitungan/MainHitungan.java
@@ -6,7 +6,12 @@ public class MainHitungan {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        
+        Hitungan h = new Hitungan();
+        int a,b,pil;
+        double x,y;
+        for (;;) {
+            
+        }
     }
     
 }
