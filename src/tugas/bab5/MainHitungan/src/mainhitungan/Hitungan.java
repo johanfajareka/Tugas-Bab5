19d1d576da8d71d5866da5c6c7409e4c79475ac9
Johan Fajar <johanrubik@gmail.com>
2016-04-26 14:29:03 +0700

    edit 1

--- src/tugas/bab5/MainHitungan/src/mainhitungan/Hitungan.java
+++ src/tugas/bab5/MainHitungan/src/mainhitungan/Hitungan.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mainhitungan;
 
-/**
- *
- * @author ASUS-A455L
- */
 public class Hitungan {
-    
+    private static double a;
+    private static int b;
+
+    public Hitungan() {
+
+    } 
 }
--- src/tugas/bab5/MainHitungan/src/mainhitungan/MainHitungan.java
+++ src/tugas/bab5/MainHitungan/src/mainhitungan/MainHitungan.java
@@ -1,21 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mainhitungan;
 
-/**
- *
- * @author ASUS-A455L
- */
+import java.util.Scanner;
+
 public class MainHitungan {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner in = new Scanner(System.in);
+        
     }
     
 }
