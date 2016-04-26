aec806ccc77a9e4cdc830908a76d81a8ee57c22b
Johan Fajar <johanrubik@gmail.com>
2016-04-26 17:22:16 +0700

    edit1

--- src/tugas/bab5/MainKomdas/src/mainkomdas/Komdas.java
+++ src/tugas/bab5/MainKomdas/src/mainkomdas/Komdas.java
@@ -1,5 +1,18 @@
 package mainkomdas;
 
 public class Komdas {
-    
+
+    private final int a = 100000;
+    private final int b = 125000;
+    private final int c = 175000;
+
+    public static void hitungJaketA(int jml) {
+        if (jml > 100) {
+            int harga = jml * 95000;
+            System.out.println("Total harga jaket A = Rp " + harga);
+        } else {
+            int harga = jml * 100000;
+            System.out.println("Total harga jaket A = Rp " + harga);
+        }
+    }
 }
