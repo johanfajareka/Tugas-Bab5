4f6ef6a7caccfedc81f68f513ee56805d06a9915
Johan Fajar <johanrubik@gmail.com>
2016-04-26 14:53:54 +0700

    Soal2

---
+++ src/tugas/bab5/MainKomdas/src/mainkomdas/Komdas.java
@@ -0,0 +1,5 @@
+package mainkomdas;
+
+public class Komdas {
+    
+}
---
+++ src/tugas/bab5/MainKomdas/src/mainkomdas/MainKomdas.java
@@ -0,0 +1,11 @@
+package mainkomdas;
+
+public class MainKomdas {
+
+    public static void main(String[] args) {
+        
+        
+        
+    }
+    
+}
